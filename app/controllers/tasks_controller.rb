6920a75d6a60b92414204aab10de6d50569ad743
lynnchang <tv725987@gmail.com>
2020-06-23 00:43:14 +0800

    add index,new,create action and view (WIP)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,29 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      redirect_to tasks_path, notice: '新增成功!'
+    else
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, 
+                                 :description, 
+                                 :status,
+                                 :priority,
+                                 :start_at,
+                                 :end_at) 
+  end
 end
