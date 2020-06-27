03ad40171aeb5b6346646adacb13ebcdc3413742
lynnchang <tv725987@gmail.com>
2020-06-27 18:59:42 +0800

    code refactor

--- spec/requests/tasks_request_spec.rb
+++ spec/requests/tasks_request_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
-  describe 'GET #index' do
-    it "should assign all tasks to @tasks" do
-        task = Task.create(
+
+  let!(:task) {
+                Task.create(
                             {
                               title:'寫測試', 
                               description:'寫新增任務測試', 
@@ -13,12 +13,18 @@ RSpec.describe "Tasks", type: :request do
                               end_at: '2020-06-26 00:44:00',
                               deleted_at: nil
                             }
-                          )
+                          )    
+              }
+  
+  describe 'GET #index' do
+    it "should assign all tasks to @tasks" do
+
         get tasks_path
         expect(assigns(:tasks)).to eq [task]
     end
 
     it "should render the index template" do
+
       get tasks_path
       expect(response).to render_template("index")
     end
@@ -26,6 +32,7 @@ RSpec.describe "Tasks", type: :request do
 
   describe 'GET #new' do
     it 'should assign a new task to @task ' do
+
       get new_task_path
       expect(assigns(:task)).to be_a_new(Task)
     end
@@ -33,8 +40,8 @@ RSpec.describe "Tasks", type: :request do
 
   describe 'POST #create' do
     context 'with valid params' do
-      it 'should create a task' do
-        valid_task_params = { 
+      before(:each) do
+                @valid_task_params = { 
                               task: 
                                 {
                                   title:'寫測試', 
@@ -44,27 +51,20 @@ RSpec.describe "Tasks", type: :request do
                                   start_at: '2020-06-25 00:44:00',
                                   end_at: '2020-06-26 00:44:00'
                                 }
-                            }                
+                            } 
+      end
+
+      it 'should create a task' do
+
         expect {
                 post tasks_path, 
-                params: valid_task_params
+                params: @valid_task_params
                }.to change { Task.count }.by(1)                 
       end
 
       it 'should redirect to root path' do
-        valid_task_params = { 
-                              task: 
-                                {
-                                  title:'寫測試', 
-                                  description:'寫新增任務測試', 
-                                  status: '待處理',
-                                  priority: '高',
-                                  start_at: '2020-06-25 00:44:00',
-                                  end_at: '2020-06-26 00:44:00'
-                                }
-                            }                
-        
-        post tasks_path, params: valid_task_params            
+
+        post tasks_path, params: @valid_task_params            
 
         expect(response).to redirect_to root_path
         expect(flash[:notice]).to eq '新增成功!'
@@ -72,39 +72,31 @@ RSpec.describe "Tasks", type: :request do
     end
 
     context 'with invalid params' do
+      before(:each) do
+        @invalid_task_params = { 
+                      task: 
+                        {
+                          title:nil, 
+                          description:'寫新增任務測試', 
+                          status: '待處理',
+                          priority: '高',
+                          start_at: '2020-06-25 00:44:00',
+                          end_at: '2020-06-26 00:44:00'
+                        }
+                    } 
+      end
+
       it 'should not create a task' do
-        invalid_task_params = { 
-          task: 
-            {
-              title: nil, 
-              description:'寫新增任務測試', 
-              status: '待處理',
-              priority: '高',
-              start_at: '2020-06-25 00:44:00',
-              end_at: '2020-06-26 00:44:00'
-            }
-        }  
 
         expect { 
                 post tasks_path, 
-                params: invalid_task_params
+                params: @invalid_task_params
               }.to change { Task.count }.by(0)
       end
 
       it 'should render edit template' do
-        invalid_task_params = { 
-          task: 
-            {
-              title: nil, 
-              description:'寫新增任務測試', 
-              status: '待處理',
-              priority: '高',
-              start_at: '2020-06-25 00:44:00',
-              end_at: '2020-06-26 00:44:00'
-            }
-        } 
 
-        post tasks_path, params: invalid_task_params
+        post tasks_path, params: @invalid_task_params
         expect(response).to render_template(:edit)
       end
     end
@@ -112,17 +104,7 @@ RSpec.describe "Tasks", type: :request do
 
   describe 'GET #show' do
     it "should assign a task to @task " do
-      task = Task.create(
-                          {
-                            title:'寫測試', 
-                            description:'寫新增任務測試', 
-                            status: '待處理',
-                            priority: '高',
-                            start_at: '2020-06-25 00:44:00',
-                            end_at: '2020-06-26 00:44:00',
-                            deleted_at: nil
-                          }
-                        )
+      
       get task_path(task)
       expect(assigns(:task)).to eq task
     end
@@ -130,67 +112,32 @@ RSpec.describe "Tasks", type: :request do
 
   describe 'GET #edit' do
     it "should assign a task to @task " do
-      task = Task.create(
-                          {
-                            title:'寫測試', 
-                            description:'寫新增任務測試', 
-                            status: '待處理',
-                            priority: '高',
-                            start_at: '2020-06-25 00:44:00',
-                            end_at: '2020-06-26 00:44:00',
-                            deleted_at: nil
-                          }
-                        )
-      get task_path(task)
+
+      get edit_task_path(task)
       expect(assigns(:task)).to eq task
     end
   end
 
   describe 'PUT #update' do
     context 'with new valid params' do
-      it 'should update one or more new attributes to @task' do
-        task = Task.create(
-                            {
-                              title:'寫測試', 
-                              description:'寫新增任務測試', 
-                              status: '待處理',
-                              priority: '高',
-                              start_at: '2020-06-25 00:44:00',
-                              end_at: '2020-06-26 00:44:00',
-                              deleted_at: nil
-                            }
-                          )
-                          
-        new_valid_params = 
+      before(:each) do
+        @new_valid_params = 
                           { 
                             task: { status: '處理中' }
                           }  
+      end
+
+      it 'should update one or more new attributes to @task' do                        
         
-        put task_path(task), params: new_valid_params
+        put task_path(task), params: @new_valid_params
         task.reload
 
-        expect(task.status).to eq new_valid_params[:task][:status]
+        expect(task.status).to eq @new_valid_params[:task][:status]
       end
 
       it 'should redirect to show template' do
-        task = Task.create(
-                            {
-                              title:'寫測試', 
-                              description:'寫新增任務測試', 
-                              status: '待處理',
-                              priority: '高',
-                              start_at: '2020-06-25 00:44:00',
-                              end_at: '2020-06-26 00:44:00',
-                              deleted_at: nil
-                            }
-                          )
-                          
-        new_valid_params = 
-                          { 
-                            task: { status: '處理中' }
-                          }  
-        
-        put task_path(task), params: new_valid_params          
+
+        put task_path(task), params: @new_valid_params          
 
         expect(response).to redirect_to task_path(task)
         expect(flash[:notice]).to eq '更新成功!'
@@ -200,33 +147,11 @@ RSpec.describe "Tasks", type: :request do
 
   describe 'DELETE #destroy' do
     it 'should delete @task' do
-      task = Task.create(
-                          {
-                            title:'寫測試', 
-                            description:'寫新增任務測試', 
-                            status: '待處理',
-                            priority: '高',
-                            start_at: '2020-06-25 00:44:00',
-                            end_at: '2020-06-26 00:44:00',
-                            deleted_at: nil
-                          }
-                        )
-      
+
       expect { delete task_path(task) }.to change { Task.count }.by(-1)
     end
 
     it 'should redirect to index template' do
-      task = Task.create(
-                          {
-                            title:'寫測試', 
-                            description:'寫新增任務測試', 
-                            status: '待處理',
-                            priority: '高',
-                            start_at: '2020-06-25 00:44:00',
-                            end_at: '2020-06-26 00:44:00',
-                            deleted_at: nil
-                          }
-                        )
       
       delete task_path(task)                  
 
