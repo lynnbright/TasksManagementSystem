a66a768fdc398bc7d5a90babc92c4cd1109e1351
lynnchang <tv725987@gmail.com>
2020-07-21 00:27:48 +0800

    Reduce repetitive code by creating two new methods

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -16,25 +16,31 @@ RSpec.feature "Tasks", driver: :selenium_chrome, js: true, type: :feature do
     
     task = Task.last
 
-    expect(current_url).to eq(root_path)
+    expect(current_url) == root_path
     expect(page).to have_text '新增成功!'
-    expect(task.title).to eq 'task title'
-    expect(task.description).to eq 'task description'
-    expect(task.status).to eq '待處理'
-    expect(task.priority).to eq '中'
+    expect_task_attributes_eq(
+      task, 
+      title: 'task title', 
+      description: 'task description', 
+      status: '待處理', 
+      priority: '中'
+    )
   end
 
   scenario "User reads the task" do
     visit root_path
     task = Task.last
-    click_link "#{task.title}"
+    find(:xpath, "//a[@href='#{task_path(task.id)}']", text: "#{task.title}").click
 
-    expect(page).to have_text("#{task.title}")
-    expect(page).to have_text("#{task.description}")
-    expect(page).to have_text("#{task.start_at}")
-    expect(page).to have_text("#{task.end_at}")
-    expect(page).to have_text("#{task.status}")
-    expect(page).to have_text("#{task.priority}")
+    expect_task_contents_eq(
+      task, 
+      title: "#{task.title}", 
+      description: "#{task.description}", 
+      start_at: "#{task.start_at}", 
+      end_at: "#{task.end_at}", 
+      status: "#{task.status}", 
+      priority: "#{task.priority}" 
+    )
   end
 
   scenario "User updates the task" do
@@ -45,7 +51,7 @@ RSpec.feature "Tasks", driver: :selenium_chrome, js: true, type: :feature do
     click_button '更新'
     task.reload
 
-    expect(current_url).to eq(task_path(task.id))
+    expect(current_url) == task_path(task.id)
     expect(page).to have_text '更新成功!'
     expect(page).to have_text("#{task.title}")
   end
@@ -56,9 +62,24 @@ RSpec.feature "Tasks", driver: :selenium_chrome, js: true, type: :feature do
     find(:xpath, "//a[@href='#{task_path(task.id)}']", text: "刪除").click
     accept_alert(text: 'Are you sure?') 
 
-    expect(current_url).to eq(root_path)
+    expect(current_url) == root_path
     expect(page).to have_text '已刪除!'
     expect(page).to have_no_xpath("//a[@href='#{task_path(task.id)}']")
+  end
+end
+
+private
+def expect_task_attributes_eq(task, values = {})
+  values.each do |k, v|
+    task_value = task.try(k.to_sym)
+    expect(task_value).to eq(v)
+  end
+end
+
+def expect_task_contents_eq(task, values = {})
+  values.each do |k, v|
+    view_task_contents = task.try(k.to_sym)
+    expect(view_task_contents).to have_text(v)
   end
 end
 
