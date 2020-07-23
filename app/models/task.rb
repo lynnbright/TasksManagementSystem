4f35b4801d10f31033b040efd6d68dca40e71c3a
lynnchang <tv725987@gmail.com>
2020-07-24 01:33:07 +0800

    WIP-Setting enum

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,15 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :priority, presence: true
   validates :status, presence: true
+
+  enum status: { I18n.t("status.ToDo") => 0, I18n.t("status.Doing") => 1, I18n.t("status.Done") => 2 }
+  enum priority: { I18n.t("priority.low") => 0, I18n.t("priority.mid")=> 1, I18n.t("priority.high") => 2 }
+
+  def self.status_options
+    self.statuses.invert.to_a
+  end
+
+  def self.priority_options
+    self.priorities.invert.to_a
+  end
 end
