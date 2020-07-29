5a8c62b09099e9cbb36c49b5eddfb956c50f7b6c
lynnchang <tv725987@gmail.com>
2020-07-30 00:25:43 +0800

    Revise enum writing by replacing I18n with pure english text and put the reusable methods into 'tasks_helper'

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,14 +3,6 @@ class Task < ApplicationRecord
   validates :priority, presence: true
   validates :status, presence: true
 
-  enum status: { I18n.t("status.ToDo") => 0, I18n.t("status.Doing") => 1, I18n.t("status.Done") => 2 }
-  enum priority: { I18n.t("priority.low") => 0, I18n.t("priority.mid")=> 1, I18n.t("priority.high") => 2 }
-
-  def self.status_options
-    self.statuses.keys
-  end
-
-  def self.priority_options
-    self.priorities.keys
-  end
+  enum status: { ToDo: 0, Doing: 1, Done: 2 }
+  enum priority: { Low: 0, Mid: 1, High: 2 }
 end
