2c4ed12d383cd254ac63c2135243e9d8ac336b84
lynnchang <tv725987@gmail.com>
2020-07-31 00:45:48 +0800

    1. Add new validation to Task Model 2. Replace input element which is datetime type with view helper ‘f.datetime_select’

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,10 @@
 class Task < ApplicationRecord
   validates :title, presence: true
-  validates :priority, presence: true
-  validates :status, presence: true
+  validates :status, presence: true, inclusion: { in: %w(ToDo Doing Done) }
+  validates :priority, presence: true, inclusion: { in: %w(Low Mid High) }
+  validates :start_at, presence: true
+  validates :end_at, presence: true
+  validates_with TaskValidator 
 
   enum status: { todo: 0, doing: 1, done: 2 }
   enum priority: { low: 0, mid: 1, high: 2 }
